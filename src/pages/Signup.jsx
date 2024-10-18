1c26d62905c07d19b7a19a293dca1d1b40b2a3cd
Mohd Arman <armansheakh987@gmail.com>
2024-10-18 13:05:44 +0530

    Login Google authetication updated

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import API_URL from './config';
 
 const Signup = () => {
     const navigate = useNavigate();
