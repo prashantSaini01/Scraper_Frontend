5ea53906bdd7f938e137bb086ec42a13a9c94e38
Mohd Arman <armansheakh987@gmail.com>
2024-10-30 11:10:56 +0530

    UI Enhaned and  Login Authentication

--- src/pages/config.js
+++ src/pages/config.js
@@ -2,8 +2,12 @@
 
 // ECS deployed Environment
 //const API_URL = "http://35.154.171.198:5000";
-// const API_URL = "https://scrap-bot-j67k.onrender.com"
+const API_URL = "https://scrap-bot-j67k.onrender.com"
 // Local Environment setup
-const API_URL = "http://127.0.0.1:5000";
+// const API_URL = "http://127.0.0.1:5000";
 
 export default API_URL;
+
+// const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
+
+// export default API_URL;
